20c915219ba69862fd246c526b752fd3ee4a96a1
Ziggy <ziggy@Ziggys-MacBook-Pro.local>
2025-08-25 12:43:05 +0100

    WIP: saving local changes before switching branch

--- ios/vapeApp/AppDelegate.swift
+++ ios/vapeApp/AppDelegate.swift
@@ -38,11 +38,11 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
     self.bundleURL()
   }
 
-  override func bundleURL() -> URL? {
+override func bundleURL() -> URL? {
 #if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    return URL(string: "http://localhost:8082/index.bundle?platform=ios&dev=true")
 #else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
-  }
+}
 }
